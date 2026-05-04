a3cd02b7d54a6fecd7f0e652c30df5764686df0e
franciscaperezca <francisca.perez.ca@ug.uchile.cl>
2026-05-04 00:29:00 -0400

    Arregla un bug que no permitia sacar bien los resultados ahora si

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 #include <stdlib.h>
 #include <time.h>
 #include "Estructuras.h"
@@ -14,6 +16,7 @@ float tamaños[] = {0.0025, 0.005, 0.01, 0.025, 0.05};
 int main(void) {
     printf("Hello, World!\n");
 
+
     FILE *out = fopen("resultados.csv", "w");
     if (!out) {
         printf("Error creando resultados.csv\n");
@@ -21,46 +24,52 @@ int main(void) {
     }
 
     fprintf(out, "N,estructura,dataset,s,puntos,IO,std_puntos,tiempo\n");
-    fflush(out);
 
-    // NEAREST X RANDOM
+    //EXPERIMENTACION NEAREST X CON DATOS RANDOM
     for (int exp = 15; exp <= 24; exp++) {
 
         int N = 1 << exp;
+
         printf("Probando (random) N = %d\n", N);
 
         hijo *puntos = leerDatos("random.bin", N);
-        if (!puntos) {
-            printf("Error leyendo datos\n");
+
+        inicializarArbol();
+
+        clock_t inicio = clock();
+
+        nearestX(puntos, N);
+
+        clock_t fin = clock();
+
+        double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
+        // guardar en disco
+        guardarArbol("rtree.bin");
+
+        FILE *f = fopen("rtree.bin", "rb");
+
+        if (!f) {
+            printf("Error abriendo archivo\n");
             return 1;
         }
 
-        inicializarArbol();
-
-        clock_t inicio = clock();
-        nearestX(puntos, N);
-        clock_t fin = clock();
-
-        double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
-
-        guardarArbol("rtree.bin");
-
-        FILE *f = fopen("rtree.bin", "rb");
-        if (!f) return 1;
-
+        // probar queries
         for (int t = 0; t < 5; t++) {
 
             float s = tamaños[t];
+
             double io_vals[100];
             double pts_vals[100];
 
+
             for (int q = 0; q < num_queries; q++) {
-                fseek(f, 0, SEEK_SET);
 
                 MBR query = generarQuery(s);
                 int io = 0;
 
-                int encontrados = rangeQuery(f, 1, query, &io);
+                int encontrados = rangeQuery(f, 0, query, &io);
 
                 io_vals[q] = io;
                 pts_vals[q] = encontrados;
@@ -76,32 +85,39 @@ int main(void) {
             double avg_io = sum_io / num_queries;
             double avg_pts = sum_pts / num_queries;
             double sd_pts = stddev(pts_vals, num_queries, avg_pts);
+
+            printf("s=%.4f -> pts=%.2f (std=%.2f), IO=%.2f\n",
+                   s, avg_pts, sd_pts, avg_io);
 
             fprintf(out, "%d,nearestX,random,%.4f,%.2f,%.2f,%.2f,%.6f\n",
                     N, s, avg_pts, avg_io, sd_pts, tiempo);
-            fflush(out);
         }
 
         fclose(f);
+
         free(puntos);
     }
 
-    // NEAREST X EUROPA
+    //EXPERIMENTACION NEAREST X CON EUROPA
     for (int exp = 15; exp <= 24; exp++) {
 
         int N = 1 << exp;
+
         printf("Probando (europa) N = %d\n", N);
 
         hijo *puntos = leerDatos("europa.bin", N);
-        if (!puntos) return 1;
 
         inicializarArbol();
 
         clock_t inicio = clock();
+
         nearestX(puntos, N);
+
         clock_t fin = clock();
 
         double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
 
         guardarArbol("rtree.bin");
 
@@ -111,16 +127,16 @@ int main(void) {
         for (int t = 0; t < 5; t++) {
 
             float s = tamaños[t];
+
             double io_vals[100];
             double pts_vals[100];
 
             for (int q = 0; q < num_queries; q++) {
-                fseek(f, 0, SEEK_SET);
 
                 MBR query = generarQuery(s);
                 int io = 0;
 
-                int encontrados = rangeQuery(f, 1, query, &io);
+                int encontrados = rangeQuery(f, 0, query, &io);
 
                 io_vals[q] = io;
                 pts_vals[q] = encontrados;
@@ -137,12 +153,16 @@ int main(void) {
             double avg_pts = sum_pts / num_queries;
             double sd_pts = stddev(pts_vals, num_queries, avg_pts);
 
+            printf("s=%.4f -> pts=%.2f (std=%.2f), IO=%.2f\n",
+                   s, avg_pts, sd_pts, avg_io);
+
             fprintf(out, "%d,nearestX,europa,%.4f,%.2f,%.2f,%.2f,%.6f\n",
                     N, s, avg_pts, avg_io, sd_pts, tiempo);
-            fflush(out);
         }
 
         fclose(f);
+
+
         free(puntos);
     }
 
@@ -150,20 +170,26 @@ int main(void) {
     for (int exp = 15; exp <= 24; exp++) {
 
         int N = 1 << exp;
+
         printf("Probando STR (random) N = %d\n", N);
 
         hijo *puntos = leerDatos("random.bin", N);
-        if (!puntos) return 1;
 
         inicializarArbol();
 
         clock_t inicio = clock();
+
         buildSTR(puntos, N);
+
         clock_t fin = clock();
 
         double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
 
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
+
+        int root = buildSTR(puntos, N);
         guardarArbol("str.bin");
+
 
         FILE *f = fopen("str.bin", "rb");
         if (!f) return 1;
@@ -171,16 +197,16 @@ int main(void) {
         for (int t = 0; t < 5; t++) {
 
             float s = tamaños[t];
+
             double io_vals[100];
             double pts_vals[100];
 
             for (int q = 0; q < num_queries; q++) {
-                fseek(f, 0, SEEK_SET);
 
                 MBR query = generarQuery(s);
                 int io = 0;
 
-                int encontrados = rangeQuery(f, 1, query, &io);
+                int encontrados = rangeQuery(f, root, query, &io);
 
                 io_vals[q] = io;
                 pts_vals[q] = encontrados;
@@ -197,12 +223,15 @@ int main(void) {
             double avg_pts = sum_pts / num_queries;
             double sd_pts = stddev(pts_vals, num_queries, avg_pts);
 
+            printf("s=%.4f -> pts=%.2f (std=%.2f), IO=%.2f\n",
+                   s, avg_pts, sd_pts, avg_io);
+
             fprintf(out, "%d,STR,random,%.4f,%.2f,%.2f,%.2f,%.6f\n",
                     N, s, avg_pts, avg_io, sd_pts, tiempo);
-            fflush(out);
         }
 
         fclose(f);
+
         free(puntos);
     }
 
@@ -210,19 +239,24 @@ int main(void) {
     for (int exp = 15; exp <= 24; exp++) {
 
         int N = 1 << exp;
+
         printf("Probando STR (europa) N = %d\n", N);
 
         hijo *puntos = leerDatos("europa.bin", N);
-        if (!puntos) return 1;
 
         inicializarArbol();
 
         clock_t inicio = clock();
+
         buildSTR(puntos, N);
+
         clock_t fin = clock();
 
         double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
 
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
+
+        int root = buildSTR(puntos, N);
         guardarArbol("str.bin");
 
         FILE *f = fopen("str.bin", "rb");
@@ -231,16 +265,16 @@ int main(void) {
         for (int t = 0; t < 5; t++) {
 
             float s = tamaños[t];
+
             double io_vals[100];
             double pts_vals[100];
 
             for (int q = 0; q < num_queries; q++) {
-                fseek(f, 0, SEEK_SET);
 
                 MBR query = generarQuery(s);
                 int io = 0;
 
-                int encontrados = rangeQuery(f, 1, query, &io);
+                int encontrados = rangeQuery(f, root, query, &io);
 
                 io_vals[q] = io;
                 pts_vals[q] = encontrados;
@@ -257,16 +291,17 @@ int main(void) {
             double avg_pts = sum_pts / num_queries;
             double sd_pts = stddev(pts_vals, num_queries, avg_pts);
 
+            printf("s=%.4f -> pts=%.2f (std=%.2f), IO=%.2f\n",
+                   s, avg_pts, sd_pts, avg_io);
+
             fprintf(out, "%d,STR,europa,%.4f,%.2f,%.2f,%.2f,%.6f\n",
                     N, s, avg_pts, avg_io, sd_pts, tiempo);
-            fflush(out);
         }
 
         fclose(f);
         free(puntos);
     }
 
-
-
     fclose(out);
+
 }
