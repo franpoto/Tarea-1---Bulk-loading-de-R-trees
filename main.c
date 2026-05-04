efb0838f8b67fb74e264796e31f21f95d3eee8a2
franciscaperezca <francisca.perez.ca@ug.uchile.cl>
2026-05-03 22:51:05 -0400

    Agrega Experimentacion de consultas para str

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "inits.h"
 #include "nearestx.h"
 #include "query.h"
+#include "str.h"
 
 
 int main(void) {
@@ -133,6 +134,123 @@ int main(void) {
 
         fclose(f);
 
+        free(puntos);
+    }
+
+    // STR RANDOM
+    for (int exp = 15; exp <= 24; exp++) {
+
+        int N = 1 << exp;
+
+        printf("Probando STR (random) N = %d\n", N);
+
+        hijo *puntos = leerDatos("random.bin", N);
+
+        inicializarArbol();
+
+        clock_t inicio = clock();
+
+        buildSTR(puntos, N);
+
+        clock_t fin = clock();
+
+        double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
+
+        guardarArbol("str.bin");
+
+        FILE *f = fopen("str.bin", "rb");
+        if (!f) {
+            printf("Error abriendo STR\n");
+            return 1;
+        }
+
+        // 🔥 queries igual que nearestX
+        for (int t = 0; t < 3; t++) {
+
+            float s = tamaños[t];
+
+            int total_io = 0;
+            int total_puntos = 0;
+
+            for (int q = 0; q < num_queries; q++) {
+
+                MBR query = generarQuery(s);
+                int io = 0;
+
+                int encontrados = rangeQuery(f, 0, query, &io);
+
+                total_io += io;
+                total_puntos += encontrados;
+            }
+
+            printf("s=%.3f -> avg puntos=%.2f, avg IO=%.2f\n",
+                   s,
+                   (double)total_puntos / num_queries,
+                   (double)total_io / num_queries);
+        }
+
+        fclose(f);
+
+        free(puntos);
+    }
+
+    // STR EUROPA
+    for (int exp = 15; exp <= 24; exp++) {
+
+        int N = 1 << exp;
+
+        printf("Probando STR (europa) N = %d\n", N);
+
+        hijo *puntos = leerDatos("europa.bin", N);
+
+        inicializarArbol();
+
+        clock_t inicio = clock();
+
+        buildSTR(puntos, N);
+
+        clock_t fin = clock();
+
+        double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+        printf("Tiempo: %.4f segundos\n\n", tiempo);
+
+        guardarArbol("str.bin");
+
+        FILE *f = fopen("str.bin", "rb");
+        if (!f) {
+            printf("Error abriendo STR\n");
+            return 1;
+        }
+
+
+        for (int t = 0; t < 3; t++) {
+
+            float s = tamaños[t];
+
+            int total_io = 0;
+            int total_puntos = 0;
+
+            for (int q = 0; q < num_queries; q++) {
+
+                MBR query = generarQuery(s);
+                int io = 0;
+
+                int encontrados = rangeQuery(f, 0, query, &io);
+
+                total_io += io;
+                total_puntos += encontrados;
+            }
+
+            printf("s=%.3f -> avg puntos=%.2f, avg IO=%.2f\n",
+                   s,
+                   (double)total_puntos / num_queries,
+                   (double)total_io / num_queries);
+        }
+
+        fclose(f);
         free(puntos);
     }
 
